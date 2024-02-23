1836fa62a453fa49d5390eb010f39b6c2a62e499
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2024-02-23 23:43:07 +0300

    Dealing with numbers in Java

--- Main.java
+++ Main.java
@@ -5,5 +5,16 @@ public class Main {
 		System.out.println("I love Java");
 		System.out.println("It is awesome and enjoyable");
 		System.out.println("Thank you for your time");
+
+		System.out.println("This is my first Java program");
+		System.out.println("Dealing with Numbers/Integers in Java");
+		System.out.println(25089);
+		System.out.println(25089.00);
+		System.out.println(25089.00f);
+		System.out.println(25089.00d);
+		System.out.println(25 * 25);
+		System.out.println(25 * 25 - 25);
+		System.out.println(25 * 25 + 25);
+		System.out.println(25 * 25 * 25 / 25);
 	}
 }
