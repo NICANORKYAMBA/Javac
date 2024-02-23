06c64b1e70bcd825418d682d674ada1154567cfe
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2024-02-23 23:34:39 +0300

    Added more println

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("I am learning Java");
+		System.out.println("I love Java");
+		System.out.println("It is awesome and enjoyable");
+		System.out.println("Bye!");
 	}
 }
