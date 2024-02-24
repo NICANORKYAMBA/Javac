7e8247566a1757d119ec9b3cc8a4aff3026e6c50
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2024-02-24 11:38:44 +0300

    Sum and product of numbers in Java

---
+++ JavaSum.java
@@ -0,0 +1,14 @@
+class JavaSum {
+	public static void main(String[] args) {
+		int a, b, sum = 0, product = 0;
+
+		a = 10;
+		b = 35;
+
+		sum = a + b;
+		product = a * b;
+
+		System.out.println("Sum: " + sum);
+		System.out.println("Product: " + product);
+	}
+}
