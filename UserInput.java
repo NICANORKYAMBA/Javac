91a961a7b7e4b95c96143b6a2f5fee28f1a969ac
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2024-02-24 12:00:23 +0300

    Using double to calculate user input averages

--- UserInput.java
+++ UserInput.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class UserInput {
 	public static void main(String[] args) {
-		int a, b, sum = 0, product = 0;
+		double a, b, sum = 0, product = 0, avg = 0;
 
 		Scanner scan = new Scanner(System.in);
 
@@ -14,8 +14,11 @@ class UserInput {
 
 		sum = a + b;
 		product = a * b;
+		avg = sum / 2;
 
 		System.out.println("Sum of your two Numbers is: " + sum);
 		System.out.println("Product of your two Numbers is: " + product);
+
+		System.out.println("Average of your two Numbers is: " + avg);
 	}
 }
