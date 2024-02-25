3c94f066102f43ee8a30e99954d69f453ccd3f7d
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2024-02-25 22:52:54 +0300

    Java Operators

---
+++ ArithmeticOperators.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+class CFG {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+    }
+}
