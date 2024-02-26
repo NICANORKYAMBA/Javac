ea606239eba97030c4c8c39438bf5e63badd3a1b
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2024-02-26 11:10:19 +0300

    Bitwise operators

---
+++ BitwiseOperator.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class BitwiseOperator {
+    public static void main(String[] args) {
+        int d = 0b1010;
+        int e = 0b1100;
+
+        System.out.println("d & e: " + (d & e));
+        System.out.println("d | e: " + (d | e));
+        System.out.println("d ^ e: " + (d ^ e));
+        System.out.println("~d: " + (~d));
+        System.out.println("d << 2: " + (d << 2));
+        System.out.println("e >> 1: " + (e >> 1));
+        System.out.println("e >>> 1: " + (e >>> 1));
+    }
+}
