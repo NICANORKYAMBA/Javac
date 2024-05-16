677a1e14cc5e298be251faea393458e843faffeb
NIcanor Kyamba <nicanorkyamba98@gmail.com>
2024-05-16 17:39:59 +0300

    For loop implementation

---
+++ control/DoWhile.java
@@ -0,0 +1,12 @@
+class DoWhile {
+	public static void main(String[] args) {
+		int n = 10;
+
+		do {
+			System.out.println(n);
+			n--;
+		} while(n > 0);
+
+		System.out.println("Blast off!");
+	}
+}
---
+++ control/ForTick.java
@@ -0,0 +1,13 @@
+class ForTick {
+
+	public static void main(String[] args) {
+		int i;
+
+		for (i = 10; i > 0; i--) {
+			System.out.println(i);
+		}
+
+		System.out.println("Blast off!");
+	}
+
+}
