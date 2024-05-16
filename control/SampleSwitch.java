ecfa5ff77713229c6a75953b508f993718061513
NIcanor Kyamba <nicanorkyamba98@gmail.com>
2024-05-16 15:25:05 +0300

    Switch in java

---
+++ control/SampleSwitch.java
@@ -0,0 +1,43 @@
+class SampleSwitch {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			switch(i) {
+				case 0:
+					System.out.println("i is 0");
+					break;
+				case 1:
+					System.out.println("i is 1");
+					break;
+				case 2:
+					System.out.println("i is 2");
+					break;
+				case 3:
+					System.out.println("i is 3");
+					break;
+				case 4:
+					System.out.println("i is 4");
+					break;
+				case 5:
+					System.out.println("i is 5");
+					break;
+				case 6:
+					System.out.println("i is 6");
+					break;
+				case 7:
+					System.out.println("i is 7");
+					break;
+				case 8:
+					System.out.println("i is 8");
+					break;
+				case 9:
+					System.out.println("i is 9");
+					break;
+				default:
+					System.out.println("i is greater than 9");
+					break;
+			}
+		}
+	}
+
+}
