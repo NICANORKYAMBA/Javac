73615f5ee9acd8764f1735eb2d15be867f5b0827
NIcanor Kyamba <nicanorkyamba98@gmail.com>
2024-05-27 10:39:28 +0300

    String arrays demonstration

---
+++ Classes/StringArray.java
@@ -0,0 +1,9 @@
+class StringArray {
+	public static void main(String[] args) {
+		String str [] = {"One", "Two", "Three"};
+
+		for (int i = 0; i < str.length; i++) {
+			System.out.println("str[" + i + "]: " + str[i]);
+		}
+	}
+}
