be0419e73748efee74afeb9960301beeba28c4c6
NIcanor Kyamba <nicanorkyamba98@gmail.com>
2024-05-27 11:03:06 +0300

    Command line arguments: args

---
+++ Classes/CommandLine.java
@@ -0,0 +1,8 @@
+// Displays all command line arguments
+class CommandLine {
+	public static void main(String[] args) {
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("Args[" + i + "]: " + args[i]);
+		}
+	}
+}
