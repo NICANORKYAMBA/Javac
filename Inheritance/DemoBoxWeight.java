d9b249382f29f3bcefc935c456d5025f056bb465
NIcanor Kyamba <nicanorkyamba98@gmail.com>
2024-05-28 10:09:34 +0300

    Class Inheritance and extension

---
+++ Inheritance/DemoBoxWeight.java
@@ -0,0 +1,69 @@
+// This program uses inheritance to extend box
+class Box {
+	double width;
+	double height;
+	double depth;
+
+	// Construct clone of an object
+	Box(Box ob) { // pass object constructor
+		width = ob.width;
+		height = ob.height;
+		depth = ob.depth;
+	}
+
+	// Constructor used when all dimensions specified
+	Box(double w, double h, double d) {
+		width = w;
+		height = h;
+		depth = d;
+	}
+
+	// Constructor used when no dimensions specified
+	Box() {
+		width = -1;
+		height = -1;
+		depth = -1;
+	}
+
+	// Constructor used when cube is created
+	Box(double len) {
+		width = height = depth = len;
+	}
+
+	// Compute and return volume
+	double volume() {
+		return width * height * depth;
+	}
+}
+
+// Here, Box is extended to include weight.
+class BoxWeight extends Box {
+	double weight;
+
+	// Constructor for BoxWeight
+	BoxWeight(double w, double h, double d, double m) {
+		width = w;
+		height = h;
+		depth = d;
+		weight = m;
+	}
+}
+
+class DemoBoxWeight {
+	public static void main(String[] args) {
+		BoxWeight mybox1 = new BoxWeight(10, 20, 15, 34.3);
+		BoxWeight mybox2 = new BoxWeight(2, 3, 4, 0.076);
+		double vol;
+
+		vol = mybox1.volume();
+
+		System.out.println("Volume of mybox1 is " + vol);
+		System.out.println("Weight of mybox1 is " + mybox1.weight);
+		System.out.println();
+
+		vol = mybox2.volume();
+
+		System.out.println("Volume of mybox2 is " + vol);
+		System.out.println("Weight of mybox2 is " + mybox2.weight);
+	}
+}
