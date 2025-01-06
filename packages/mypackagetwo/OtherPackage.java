e9dc1afd34ddcaa69a81487ecd8ec0e0683b39bf
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2025-01-06 11:13:34 +0300

    Using two different packages to test accessbility

---
+++ packages/mypackagetwo/OtherPackage.java
@@ -0,0 +1,12 @@
+package mypackagetwo;
+
+public class OtherPackage {
+    public OtherPackage() {
+        packageone.Protection p = new packageone.Protection();
+        System.out.println("OtherPackage constructor");
+        // System.out.println("n = " + p.n); // Not accessible (default access). Only accessible within the same package
+        // System.out.println("n_pri = " + p.n_pri); // Not accessible (private access) because it is private to Protection Class and thus only accessible within Protection Class
+        // System.out.println("n_pro = " + p.n_pro); // Not accessible (protected access) because it is not a subclass of Protection Class
+        System.out.println("n_pub = " + p.n_pub); // Accessible (public access)
+    }
+}
---
+++ packages/mypackagetwo/ProtectionTwo.java
@@ -0,0 +1,11 @@
+package mypackagetwo;
+
+public class ProtectionTwo extends packageone.Protection {
+    public ProtectionTwo() {
+        System.out.println("Derived constructor");
+        // System.out.println("n = " + n); // Not accessible (default access)
+        // System.out.println("n_pri = " + n_pri); // Not accessible (private access)
+        System.out.println("n_pro = " + n_pro); // Accessible (protected access) because it is a subclass of Protection Class
+        System.out.println("n_pub = " + n_pub); // Accessible (public access)
+    }
+}
