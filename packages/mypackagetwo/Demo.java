74397304553821d0aac38733ed5853a36649f568
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2025-01-06 11:25:29 +0300

    feat: Testing the outputs of access modifiers

---
+++ packages/mypackagetwo/Demo.java
@@ -0,0 +1,8 @@
+package mypackagetwo;
+
+public class Demo {
+    public static void main(String[] args) {
+        OtherPackage op = new OtherPackage();
+        ProtectionTwo pt = new ProtectionTwo();
+    }
+}
---
+++ packages/packageone/Demo.java
@@ -0,0 +1,9 @@
+package packageone;
+
+public class Demo {
+    public static void main(String[] args) {
+        Protection p = new Protection();
+        Derived d = new Derived();
+        SamePackage sp = new SamePackage();
+    }
+}
