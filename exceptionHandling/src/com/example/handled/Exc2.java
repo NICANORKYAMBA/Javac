efa8537f36ce6acdcd76e2e38925aa60fc0804bc
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2025-01-26 15:48:48 +0300

    Feat: Using try and catch block to handle errors

---
+++ exceptionHandling/src/com/example/handled/Exc2.java
@@ -0,0 +1,14 @@
+package com.example.handled;
+
+public class Exc2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        try {
+            int c = a / b;
+            System.out.println("Result: " + c);
+        } catch (ArithmeticException e) {
+            System.out.println("An arithmetic exception occurred: " + e);
+        }
+    }
+}
