f6afa07ad31a783d2cad9fb7dc9cfdd1b1ea486b
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2025-01-26 15:43:25 +0300

    Feat: Uncaught exception

---
+++ exceptionHandling/src/com/example/uncaught/Exc0.java
@@ -0,0 +1,10 @@
+package com.example.uncaught;
+
+public class Exc0 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        int c = a / b;
+        System.out.println("Result: " + c);
+    }
+}
