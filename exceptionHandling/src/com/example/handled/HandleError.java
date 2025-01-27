174bce9312a15db234cc09ea7e262f49172ae3b9
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2025-01-27 16:39:45 +0300

    Concept: Displaying a Description of an Exception

--- exceptionHandling/src/com/example/handled/HandleError.java
+++ exceptionHandling/src/com/example/handled/HandleError.java
@@ -13,7 +13,7 @@ public class HandleError {
                 c = r.nextInt();
                 a = 12345 / (b / c);
             } catch (ArithmeticException e) {
-                System.out.println("Division by zero.");
+                System.out.println("Exception: " + e);
                 a = 0; // set a to zero and continue
             }
             System.out.println("a: " + a);
